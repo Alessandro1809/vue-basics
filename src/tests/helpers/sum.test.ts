d91d00e673637f3a1b514db1387d053df9bb6ed2
Alessandro1809 <dalessandro18dg@gmail.com>
2025-01-08 22:15:37 -0600

    agregando describe

--- src/tests/helpers/sum.test.ts
+++ src/tests/helpers/sum.test.ts
@@ -1,34 +1,39 @@
 // sum.test.js
 import { expect, test } from 'vitest';
 import { addArray, sum } from '../../helpers/sum';
+import { describe } from 'node:test';
 
-test('adds 1 + 2 to equal 3', () => {
-  //preparacion
-  const a = 1;
-  const b = 2;
-  //estimulo
-  const result = sum(a, b);
+describe('sum dunction is working done', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    //preparacion
+    const a = 1;
+    const b = 2;
+    //estimulo
+    const result = sum(a, b);
 
-  //comportamiento esperado
-  expect(result).toBe(a + b);
+    //comportamiento esperado
+    expect(result).toBe(a + b);
+  });
 });
 
-test('suma los valores del arreglo correctamente', () => {
-  //preparacion
-  const a = [1, 2, 3, 4, 5];
-  //estimulo
-  const result = addArray(a);
+describe('addArray function is working done', () => {
+  test('suma los valores del arreglo correctamente', () => {
+    //preparacion
+    const a = [1, 2, 3, 4, 5];
+    //estimulo
+    const result = addArray(a);
 
-  //comportamiento esperado
-  expect(result).toBe(15);
-});
+    //comportamiento esperado
+    expect(result).toBe(15);
+  });
 
-test('should return 0 if the array is empty', () => {
-  //preparacion
-  const arr: [] = [];
-  //estimulo
-  const result = addArray(arr);
+  test('should return 0 if the array is empty', () => {
+    //preparacion
+    const arr: [] = [];
+    //estimulo
+    const result = addArray(arr);
 
-  //comportamiento esperado
-  expect(result).toBe(0);
+    //comportamiento esperado
+    expect(result).toBe(0);
+  });
 });
